1487d87085f9bd0d556c7032282fb6b73f8c8c0b
Jacob Barnett <2818246+jacoblb64@users.noreply.github.com>
2025-09-30 15:31:22 -0400

    chore: formatting

--- src/game/libs/GameEngine.ts
+++ src/game/libs/GameEngine.ts
@@ -1,17 +1,11 @@
-
-import  mediumWords from '../../../public/assets/files/google-10000-english-usa-no-swears-medium.txt?raw'
-import  shortWords from '../../../public/assets/files/google-10000-english-usa-no-swears-short.txt?raw'
+import mediumWords from "../../../public/assets/files/google-10000-english-usa-no-swears-medium.txt?raw";
+import shortWords from "../../../public/assets/files/google-10000-english-usa-no-swears-short.txt?raw";
 interface Challenge {
     characterLength: number;
     time: number;
 }
 
-
-
-
-
-
-export function getScore(time: number, length: number){
+export function getScore(time: number, length: number) {
     return (Math.ceil(time) - 6) * length;
 }
 
@@ -21,16 +15,14 @@ export function getStrategyA(): Challenge[] {
         { characterLength: 1, time: 5 },
         { characterLength: 2, time: 5 },
         { characterLength: 3, time: 5 },
-    ]
+    ];
 }
 
 export function getStrategyB(): Challenge[] {
-
     const challenges: Challenge[] = [];
 
-    for( let j = 5; j >= 1; j--){
-        
-        for(let i = 1; i <= 5; i++){
+    for (let j = 5; j >= 1; j--) {
+        for (let i = 1; i <= 5; i++) {
             challenges.push({ characterLength: i, time: j });
         }
     }
@@ -41,25 +33,31 @@ export function getStrategyB(): Challenge[] {
         { characterLength: 1, time: 5 },
         { characterLength: 2, time: 5 },
         { characterLength: 3, time: 5 },
-    ]
+    ];
 }
-export function getNextChallenge(challengeNumber: number, ChallengeStrategy: Challenge[]){
-    
+export function getNextChallenge(
+    challengeNumber: number,
+    ChallengeStrategy: Challenge[]
+) {
     const challenge = ChallengeStrategy[challengeNumber];
     // make a call to a file to get the actual word;
-    let words
-    if(challenge.characterLength > 4){
+    let words;
+    if (challenge.characterLength > 4) {
         words = mediumWords;
     } else {
         words = shortWords;
     }
 
-    const parsedWords = words.split('\n').filter((word: string) => word.length === challenge.characterLength);
-    const randomWord = parsedWords[Math.floor(Math.random() * parsedWords.length)];
+    const parsedWords = words
+        .split("\n")
+        .filter((word: string) => word.length === challenge.characterLength);
+    const randomWord =
+        parsedWords[Math.floor(Math.random() * parsedWords.length)];
 
     return {
         characterLength: challenge.characterLength,
         time: challenge.time,
         word: randomWord,
-    }
+    };
 }
+
