959142d62de329020001c0966819b673405643cc
evhabs <evanhabersham@gmail.com>
2025-09-30 15:22:57 -0400

    feat: added game engine

---
+++ src/game/libs/GameEngine.ts
@@ -0,0 +1,65 @@
+
+import  mediumWords from '../../../public/assets/files/google-10000-english-usa-no-swears-medium.txt?raw'
+import  shortWords from '../../../public/assets/files/google-10000-english-usa-no-swears-short.txt?raw'
+interface Challenge {
+    characterLength: number;
+    time: number;
+}
+
+
+
+
+
+
+export function getScore(time: number, length: number){
+    return (Math.ceil(time) - 6) * length;
+}
+
+// gets selected by user once they start everything up;
+export function getStrategyA(): Challenge[] {
+    return [
+        { characterLength: 1, time: 5 },
+        { characterLength: 2, time: 5 },
+        { characterLength: 3, time: 5 },
+    ]
+}
+
+export function getStrategyB(): Challenge[] {
+
+    const challenges: Challenge[] = [];
+
+    for( let j = 5; j >= 1; j--){
+        
+        for(let i = 1; i <= 5; i++){
+            challenges.push({ characterLength: i, time: j });
+        }
+    }
+    console.log(challenges);
+    return challenges;
+
+    return [
+        { characterLength: 1, time: 5 },
+        { characterLength: 2, time: 5 },
+        { characterLength: 3, time: 5 },
+    ]
+}
+export function getNextChallenge(challengeNumber: number, ChallengeStrategy: Challenge[]){
+    
+    const challenge = ChallengeStrategy[challengeNumber];
+    // make a call to a file to get the actual word;
+    let words
+    if(challenge.characterLength > 4){
+        words = mediumWords;
+    } else {
+        words = shortWords;
+    }
+
+    const parsedWords = words.split('\n').filter((word: string) => word.length === challenge.characterLength);
+    const randomWord = parsedWords[Math.floor(Math.random() * parsedWords.length)];
+
+    return {
+        characterLength: challenge.characterLength,
+        time: challenge.time,
+        word: randomWord,
+    }
+}
--- src/game/scenes/MainMenu.ts
+++ src/game/scenes/MainMenu.ts
@@ -1,6 +1,7 @@
 import { GameObjects, Scene } from 'phaser';
 
 import { EventBus } from '../EventBus';
+import { getNextChallenge, getStrategyA, getStrategyB } from '../libs/gameEngine';
 
 export class MainMenu extends Scene
 {
@@ -17,7 +18,9 @@ export class MainMenu extends Scene
     create ()
     {
         this.background = this.add.image(512, 384, 'background');
-
+       const myStrategy = getStrategyB();
+      const nextChallenge = getNextChallenge(0, myStrategy);
+      console.log(nextChallenge);
         this.logo = this.add.image(512, 300, 'logo').setDepth(100);
 
         this.title = this.add.text(512, 460, 'Main Menu', {
