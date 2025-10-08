ec188547165b8f12c23a2faf3c79f51ccca72017
Jacob Barnett <2818246+jacoblb64@users.noreply.github.com>
2025-10-07 23:22:39 -0400

    feat: mostly playable game, with timers, scoring, and appropriate words! (#4)
    * feat: Make everything more playable
    
    - Filter out words with repeated characters
    - Make the game strategy longer (still errors at the end)
    - Update html title
    - Scoring! Simple incrementing for now
    - Explainer text that goes away on first valid keydown
    
    * feat: working timer and gameplay loop
    
    - added a timer to the top right corner
    - when timer expires, if you're still on the same challenge, go to game over
    - updated game over screen
    - known bug: restarting from gameover just continues, doesn't reset word and score
    
    * feat: dynamic scoring
    
    * fix: boot issue
    
    * feat: more visible timer and slighly easier timings
    
    - moved the timer farther in (75 instead of 32px)
    - made the timer bigger
    - started from 6 seconds
    - "expert mode" is now 3 seconds instead of 1

--- src/game/scenes/Game.ts
+++ src/game/scenes/Game.ts
@@ -1,90 +1,164 @@
-import { EventBus } from '../EventBus';
-import { Scene } from 'phaser';
+import { EventBus } from "../EventBus";
+import { GameObjects, Scene, Time } from "phaser";
 
-import { Challenge, getNextChallenge, getStrategyB } from '../libs/GameEngine';
+import {
+    Challenge,
+    getNextChallenge,
+    getScore,
+    getStrategyB,
+} from "../libs/GameEngine";
 
-export class Game extends Scene
-{
+export class Game extends Scene {
     camera: Phaser.Cameras.Scene2D.Camera;
     background: Phaser.GameObjects.Image;
     gameText: Phaser.GameObjects.Text;
-    
+    explainer: GameObjects.Text;
+
     keysDown: Set<string>;
     activeTextObjects: Record<string, Phaser.GameObjects.Text>;
     isMouseDown: boolean;
     challengeNumber: number;
+    score: number;
+    timer: Time.TimerEvent;
+    timeText: GameObjects.Text;
 
-    mockString: string;
+    curChallengeText: string;
     gameStrategy: Challenge[];
 
-    constructor ()
-    {
-        super('Game');
+    explainerText = `
+        Keyboard Twister!
+        Hold your mouse clicked with one hand, don't use it!
+        With your other hand, press all of the letters of the current word
+    `;
+
+    constructor() {
+        super("Game");
         this.activeTextObjects = {};
         this.isMouseDown = false;
         this.keysDown = new Set();
         this.gameStrategy = getStrategyB();
-        this.challengeNumber = 0
-        this.mockString = getNextChallenge(this.challengeNumber++, this.gameStrategy).word
-        console.log('Challenge:', this.mockString)
-        EventBus.emit('update-word', this.mockString)
+        this.challengeNumber = 0;
+        this.score = 0;
     }
 
     checkAgainstChallenge(key: string) {
-        if (this.mockString.indexOf(key) > -1) {
+        if (this.curChallengeText.indexOf(key) > -1) {
             if (!this.activeTextObjects[key]) {
-                this.activeTextObjects[key] = this.add.text(512 * Math.random(), 384 * Math.random(), key, {
-                    fontSize: '36pt'
-                })
+                this.activeTextObjects[key] = this.add.text(
+                    512 * Math.random(),
+                    384 * Math.random(),
+                    key,
+                    {
+                        fontSize: "36pt",
+                    }
+                );
             }
-            this.keysDown.add(key)
-            if (this.keysDown.size === this.mockString.length) {
-                console.log("Met challenge!")
-                this.mockString = getNextChallenge(this.challengeNumber++, this.gameStrategy).word
-                this.keysDown = new Set()
-                for(const key in this.activeTextObjects) {
-                    this.activeTextObjects[key].destroy()
+            this.keysDown.add(key);
+            if (this.keysDown.size === this.curChallengeText.length) {
+                console.log("Met challenge!");
+
+                let scoreIncrement = 1;
+                if (this.timer) {
+                    scoreIncrement = getScore(
+                        this.timer.getElapsedSeconds(),
+                        this.curChallengeText.length
+                    );
                 }
-                this.activeTextObjects = {}
-                console.log("New challenge: ", this.mockString)
-                EventBus.emit('update-word', this.mockString)
+
+                let next = getNextChallenge(
+                    this.challengeNumber++,
+                    this.gameStrategy
+                );
+                this.curChallengeText = next.word;
+                this.timer = this.time.addEvent({
+                    delay: next.time * 1000,
+                    callback: this.timerUp,
+                    callbackScope: this,
+                    args: [this.challengeNumber],
+                });
+
+                this.keysDown = new Set();
+                for (const key in this.activeTextObjects) {
+                    this.activeTextObjects[key].destroy();
+                }
+                this.activeTextObjects = {};
+
+                this.score += scoreIncrement;
+                EventBus.emit("update-score", this.score);
+
+                console.log("New challenge: ", this.curChallengeText);
+                EventBus.emit("update-word", this.curChallengeText);
             }
-            
         }
     }
 
-    create ()
-    {
+    create() {
         this.camera = this.cameras.main;
         this.camera.setBackgroundColor(0x00ff00);
 
-        this.background = this.add.image(512, 384, 'background');
+        this.background = this.add.image(512, 384, "background");
         this.background.setAlpha(0.5);
 
-        EventBus.emit('current-scene-ready', this);
+        this.explainer = this.add
+            .text(450, 300, this.explainerText, {
+                fontSize: 24,
+                color: "#ffffff",
+                stroke: "#000000",
+                strokeThickness: 2,
+                align: "center",
+            })
+            .setOrigin(0.5)
+            .setDepth(100);
 
-        this.input.keyboard?.on('keydown', (event: KeyboardEvent) => {
-            if (!this.isMouseDown) return
-            this.checkAgainstChallenge(event.key)
+        this.timeText = this.add
+            .text(1024 - 75, 75, "Time: 5", { fontSize: 20 })
+            .setOrigin(1, 0)
+            .setDepth(1);
 
-        })
-        this.input.keyboard?.on('keyup', (event: KeyboardEvent) => {
-            if (!this.isMouseDown) return
-            const key = event.key
+        this.curChallengeText = getNextChallenge(
+            this.challengeNumber++,
+            this.gameStrategy
+        ).word;
+        console.log("Challenge:", this.curChallengeText);
+        EventBus.emit("update-word", this.curChallengeText);
+
+        EventBus.emit("current-scene-ready", this);
+
+        this.input.keyboard?.on("keydown", (event: KeyboardEvent) => {
+            if (!this.isMouseDown) return;
+            this.explainer.destroy();
+            this.checkAgainstChallenge(event.key);
+        });
+        this.input.keyboard?.on("keyup", (event: KeyboardEvent) => {
+            if (!this.isMouseDown) return;
+            const key = event.key;
             if (this.activeTextObjects[key]) {
-                this.activeTextObjects[key].destroy()
-                delete(this.activeTextObjects[key])
+                this.activeTextObjects[key].destroy();
+                delete this.activeTextObjects[key];
             }
-            this.keysDown.delete(key)
-        })
+            this.keysDown.delete(key);
+        });
     }
 
-    update () {
-        this.isMouseDown = this.input.activePointer.isDown
+    update() {
+        this.isMouseDown = this.input.activePointer.isDown;
+        if (this.timer) {
+            this.timeText.setText(
+                "Time: " + Math.ceil(this.timer.getRemainingSeconds())
+            );
+        }
     }
 
-    changeScene ()
-    {
-        this.scene.start('GameOver');
+    changeScene() {
+        this.scene.start("GameOver");
+    }
+
+    timerUp(args: any) {
+        // if the player is still on the same challenge that the timer was created at, game over
+        if (args && args >= this.challengeNumber) {
+            this.registry.set("score", this.score);
+            this.changeScene();
+        }
     }
 }
+
